faad88ad76d77da62a7458084dc4ff2410be9bd0
karahman <jakarahman007@gmail.com>
2021-01-24 17:38:22 +0700

    add menus table & model

---
+++ database/migrations/2021_01_24_173310_create_menus_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMenusTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('menus', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('parent_id')->nullable();
+            $table->string('name');
+            $table->string('icon');
+            $table->string('path')->unique();
+            $table->enum('has_sub_menus', ['Y', 'N'])->default('N');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('menus');
+    }
+}
