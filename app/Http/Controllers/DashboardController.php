0ec214289f06bb44f84ddd0ae4e29d5d69d53347
karahman <jakarahman007@gmail.com>
2021-01-29 17:51:15 +0700

    add data count

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,14 +2,31 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\News;
+use App\Models\Tag;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
+    /**
+     * Show index page.
+     *
+     * @return  \Illuminate\Contracts\View\View|\Illuminate\Contracts\View\Factory
+     */
     public function index()
     {
+        $totalAdmin = User::role('super admin')->count();
+        $totalAuthor = User::role('author')->count();
+        $totalNews = News::count();
+        $totalTags = Tag::count();
+
         return view('pages.administrator.dashboard', [
-            'title' => 'Dashboard'
+            'title' => 'Dashboard',
+            'totalAdmin' => $totalAdmin,
+            'totalAuthor' => $totalAuthor,
+            'totalNews' => $totalNews,
+            'totalTags' => $totalTags,
         ]);
     }
 }
--- resources/views/pages/administrator/dashboard.blade.php
+++ resources/views/pages/administrator/dashboard.blade.php
@@ -4,15 +4,30 @@
   <div class="row">
     <div class="col-lg-3 col-md-6 col-sm-6 col-12">
       <div class="card card-statistic-1">
-        <div class="card-icon bg-primary">
-          <i class="far fa-user"></i>
+        <div class="card-icon bg-success">
+          <i class="fas fa-user"></i>
         </div>
         <div class="card-wrap">
           <div class="card-header">
             <h4>Total Admin</h4>
           </div>
           <div class="card-body">
-            10
+            {{ $totalAdmin }}
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+      <div class="card card-statistic-1">
+        <div class="card-icon bg-primary">
+          <i class="fas fa-pencil-ruler"></i>
+        </div>
+        <div class="card-wrap">
+          <div class="card-header">
+            <h4>Author</h4>
+          </div>
+          <div class="card-body">
+            {{ $totalAuthor }}
           </div>
         </div>
       </div>
@@ -27,7 +42,7 @@
             <h4>News</h4>
           </div>
           <div class="card-body">
-            42
+            {{ $totalNews }}
           </div>
         </div>
       </div>
@@ -35,29 +50,14 @@
     <div class="col-lg-3 col-md-6 col-sm-6 col-12">
       <div class="card card-statistic-1">
         <div class="card-icon bg-warning">
-          <i class="far fa-file"></i>
+          <i class="fas fa-tags"></i>
         </div>
         <div class="card-wrap">
           <div class="card-header">
-            <h4>Reports</h4>
+            <h4>Tags</h4>
           </div>
           <div class="card-body">
-            1,201
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="col-lg-3 col-md-6 col-sm-6 col-12">
-      <div class="card card-statistic-1">
-        <div class="card-icon bg-success">
-          <i class="fas fa-circle"></i>
-        </div>
-        <div class="card-wrap">
-          <div class="card-header">
-            <h4>Online Users</h4>
-          </div>
-          <div class="card-body">
-            47
+            {{ $totalTags }}
           </div>
         </div>
       </div>
