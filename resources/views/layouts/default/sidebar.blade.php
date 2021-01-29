1d6c3544764ef5e5b06a2a2a27e85e672a6ae979
karahman <jakarahman007@gmail.com>
2021-01-29 20:10:18 +0700

    set route

--- resources/views/layouts/default/sidebar.blade.php
+++ resources/views/layouts/default/sidebar.blade.php
@@ -1,10 +1,10 @@
 <div class="main-sidebar">
   <aside id="sidebar-wrapper">
     <div class="sidebar-brand">
-      <a href="index.html">{{ env('APP_NAME') }}</a>
+      <a href="{{ route('administrator.dashboard') }}">{{ env('APP_NAME') }}</a>
     </div>
     <div class="sidebar-brand sidebar-brand-sm">
-      <a href="index.html">St</a>
+      <a href="{{ route('administrator.dashboard') }}">{{ env('APP_NAME') }}</a>
     </div>
     <ul class="sidebar-menu">
       <li class="menu-header">General</li>
