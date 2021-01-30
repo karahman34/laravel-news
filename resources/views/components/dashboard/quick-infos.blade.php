403783ddacc49266631c4e16b6b541239e79a619
karahman <jakarahman007@gmail.com>
2021-01-30 07:55:33 +0700

    remove total text

--- resources/views/components/dashboard/quick-infos.blade.php
+++ resources/views/components/dashboard/quick-infos.blade.php
@@ -6,7 +6,7 @@
       </div>
       <div class="card-wrap">
         <div class="card-header">
-          <h4>Total Admin</h4>
+          <h4>Admin</h4>
         </div>
         <div class="card-body">
           {{ $totalAdmin }}
